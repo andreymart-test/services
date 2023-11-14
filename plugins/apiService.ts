6bf303202e36b3915f8501d0e2f327a2fae1221b
slay9 <slay9090@yandex.ru>
2023-11-15 02:09:06 +0300

    service api

--- plugins/apiService.ts
+++ plugins/apiService.ts
@@ -1,6 +1,6 @@
 export default defineNuxtPlugin(() => {
   const _api = {
-    getGames: async () => await useFetch('http://nasoft888.com/game-service/v1/games?limit=10&offset=0')
+    getGames: async () => await useFetch('http://nasoft888.com/game-service/v1/games?limit=5&offset=0')
   }
   return {
     provide: {
