d6a11fc661d211c24f58f7ebd28c2e2ebf976c35
slay9 <slay9090@yandex.ru>
2023-11-15 03:21:22 +0300

    gateway

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'services',
+      meta: [
+        { name: 'description', content: 'services layer' }
+      ],
+    }
+  }
 })
